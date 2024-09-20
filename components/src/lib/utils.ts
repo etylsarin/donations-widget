680d384c0f414e2cf52a49f3ea5551460947fd04
Filip Mares <maresf@gmail.com>
2024-09-20 08:22:56 +0200

    =add optional recurrence

--- components/src/lib/utils.ts
+++ components/src/lib/utils.ts
@@ -1,3 +1,5 @@
 export const camelize = (str: string) => str.replace(/-./g, x=>x[1].toUpperCase());
 
 export const formatStringNumber = (n: string) => n.replace(/(?<!\.\d+)\B(?=(\d{3})+\b)/g, " ").replace(/(?<=\.(\d{3})+)\B/g, " ");
+
+export const generateUniqueNum = () => +performance.now().toString().replace('.', '7');
