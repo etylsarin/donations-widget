a83ad1b9bc3b91d9e2820d9505453cbe41f4c051
Jaroslav Böhmer <joas1988@gmail.com>
2025-03-20 23:42:21 +0100

    Mailer with basic template

--- lambda/index.mjs
+++ lambda/index.mjs
@@ -1,6 +1,6 @@
 import { SSMClient, ListAssociationsCommand } from "@aws-sdk/client-ssm";
 import { GpWebpayRequest, GpWebpayOperation, GpWebpay, GpWebpayRequestCurrency } from "@topmonks/gpwebpay"
-import { S3Client, GetObjectCommand } from "@aws-sdk/client-s3";
+import { S3Client, GetObjectCommand, PutObjectCommand } from "@aws-sdk/client-s3";
 const ssm = new SSMClient();
 
 const input = {
@@ -16,7 +16,7 @@ const bodyString = await res.Body.transformToString();
 const config = {
   gatewayUrl: 'https://test.3dsecure.gpwebpay.com/pgw/order.do',
   privateKey: bodyString,
-  privateKeyPass: '',
+  privateKeyPass: 'dsfdsfsdfsd',
   merchantNumber: '8888890693',
   publicKey: '1234'
 }
@@ -30,7 +30,11 @@ const client = new GpWebpay(
 )
 
 export const handler = async (event, context) => {
+  if (event.httpMethod == "OPTIONS") {
+    return context.done(undefined, cors_data)
+}
   const payload = JSON.parse(event.body).parameters
+
     const request = new GpWebpayRequest(
     GpWebpayOperation.CREATE_ORDER,
     payload.orderNumber,
@@ -38,17 +42,61 @@ export const handler = async (event, context) => {
     payload.currency,
     payload.redirect_url
   )
+  await logToS3(payload, "audit_logs")
+  const response = client.getRequestUrl(request)
+  await logToS3(response, "gw_webpay_audit_log")
   return formatResponse(client.getRequestUrl(request))
+};
+
+const headers = {
+    "Content-Type": "application/json",
+    "Access-Control-Allow-Origin": "*",
+    "Cache-Control": "max-age=0, no-store, must-revalidate",
+    Pragma: "no-cache",
+    Expires: 0
+};
+
+
+
+const cors_headers = {
+    "Content-Type": "application/json",
+    "Access-Control-Allow-Headers" : "Content-Type",
+    "Access-Control-Allow-Origin": "*",
+    "Cache-Control": "max-age=0, no-store, must-revalidate",
+    Pragma: "no-cache",
+    Expires: 0
+};
+
+const cors_data = {
+        multiValueHeaders: {},
+        isBase64Encoded: false,
+        statusCode: 200,
+        headers: cors_headers,
+        body: ""
 };
 
 var formatResponse = function(body){
   var response = {
     "statusCode": 200,
     "headers": {
-      "Content-Type": "application/json"
+      "Content-Type": "application/json",
+      "Access-Control-Allow-Origin" : "*"
     },
     "isBase64Encoded": false,
     "body": body
   }
   return response
 }
+
+const logToS3 = async function (body, prefix) { // Make logToS3 async
+  const timestamp = new Date().toISOString().replace(/[:.]/g, "-");
+  const key = `${prefix}/${timestamp}.json`;
+  const input = {
+      Bucket: "mc-happy-hearts",
+      Key: key,
+      Body: JSON.stringify(body),
+  };
+  const command = new PutObjectCommand(input);
+  const response = await s3.send(command); // Use await here
+  return response;
+};
